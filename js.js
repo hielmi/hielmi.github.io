39dd9a26ef5ca69b803c091320353acabd8bd1e2
Hielmi Sulaeman <hielmisulaeman10@gmail.com>
2022-07-18 21:21:15 +0700

    inital commit

---
+++ js.js
@@ -0,0 +1,16 @@
+// var paragraph = document.querySelector('p');
+// var mediaQueryList = window.matchMedia('(max-width: 600px)');
+
+// function screenTest(e) {
+//   if (e.matches) {
+//     /* the viewport is 600 pixels wide or less */
+//     paragraph.textContent = 'This is a narrow screen — 600px wide or less.';
+//     document.body.style.backgroundColor = 'pink';
+//   } else {
+//     /* the viewport is more than than 600 pixels wide */
+//     paragraph.textContent = 'This is a wide screen — more than 600px wide.';
+//     document.body.style.backgroundColor = 'aquamarine';
+//   }
+// }
+
+// mediaQueryList.addListener(screenTest);
